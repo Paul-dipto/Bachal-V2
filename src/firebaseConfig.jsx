28e6a2e1614aadb77d6a163fadabaf2fcfef789d
Homelander-69 <abhinashpaul370@gmail.com>
2023-12-26 17:30:01 +0600

    add-on

---
+++ src/firebaseConfig.jsx
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+   apiKey: "AIzaSyAQFXI890113OdVomEbF5v0ZOX8-GYRMgQ",
+   authDomain: "bachal-v2.firebaseapp.com",
+   projectId: "bachal-v2",
+   storageBucket: "bachal-v2.appspot.com",
+   messagingSenderId: "436804054579",
+   appId: "1:436804054579:web:4ff0782d71741589b7bd46",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default firebaseConfig;
--- src/pages/Registation/Registration.jsx
+++ src/pages/Registation/Registration.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Grid, TextField, Button } from "@mui/material";
 import regimg from "../../assets/regi_img.png";
 import "../Registation/Registration.css";
 
+let initialValues = {
+   email: "",
+   fullName: "",
+   password: "",
+};
+
 const Registration = () => {
+   let [values, setValues] = useState(initialValues);
+   let handleValue = (e) => {
+      setValues({
+         ...values,
+         [e.target.name]: e.target.value,
+      });
+      console.log(values);
+   };
    return (
       <>
          <Grid container spacing={2} rowGap={0} columnGap={0}>
@@ -14,6 +28,8 @@ const Registration = () => {
                   <div className="reg_input">
                      <TextField
                         id="outlined-basic"
+                        onChange={handleValue}
+                        name="email"
                         label="Email Address"
                         variant="outlined"
                      />
@@ -21,6 +37,8 @@ const Registration = () => {
                   <div className="reg_input">
                      <TextField
                         id="outlined-basic"
+                        onChange={handleValue}
+                        name="fullName"
                         label="Full Name"
                         variant="outlined"
                      />
@@ -28,6 +46,9 @@ const Registration = () => {
                   <div className="reg_input">
                      <TextField
                         id="outlined-basic"
+                        type="password"
+                        onChange={handleValue}
+                        name="password"
                         label="Password"
                         variant="outlined"
                      />
