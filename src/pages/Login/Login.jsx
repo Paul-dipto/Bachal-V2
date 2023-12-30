ae78716ac902eb142322e9d82f86a81bd69f91e2
Homelander-69 <abhinashpaul370@gmail.com>
2023-12-31 00:29:14 +0600

    add

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,63 @@
-import React from "react";
-import { Grid, TextField, Button } from "@mui/material";
+import React, { useState } from "react";
+import {
+   Grid,
+   TextField,
+   Button,
+   CircularProgress,
+   Alert,
+} from "@mui/material";
 import "../Login/login.css";
 import login_img from "../../assets/loginjpg.jpg";
+import {
+   getAuth,
+   signInWithEmailAndPassword,
+   GoogleAuthProvider,
+   signInWithPopup,
+} from "firebase/auth";
 import google_log from "../../assets/google.png";
+import { Link } from "react-router-dom";
+import { FaRegEye, FaRegEyeSlash } from "react-icons/fa6";
+
+let initialValues = {
+   email: "",
+   password: "",
+   loading: false,
+   eye: false,
+};
 
 const Login = () => {
+   let [values, setValues] = useState(initialValues);
+   const auth = getAuth();
+   const provider = new GoogleAuthProvider();
+
+   let handleValue = (e) => {
+      setValues({
+         ...values,
+         [e.target.name]: e.target.value,
+      });
+   };
+
+   let Handlesubmit = () => {
+      let { email, password } = values;
+      setValues({
+         ...values,
+         loading: true,
+      });
+      signInWithEmailAndPassword(auth, email, password).then((user) => {
+         setValues({
+            email: "",
+            password: "",
+            loading: false,
+         });
+         // navigate("/login");
+      });
+   };
+
+   let handleGoogleLogin = () => {
+      signInWithPopup(auth, provider).then((result) => {
+         console.log(result);
+      });
+   };
    return (
       <>
          <Grid container spacing={2} rowGap={0} columnGap={0}>
@@ -16,11 +69,16 @@ const Login = () => {
                         className="google_log"
                         src={google_log}
                         alt="google_log"
+                        onClick={handleGoogleLogin}
                      />
                   </picture>
                   <div className="log_input">
                      <TextField
                         id="outlined-basic"
+                        onChange={handleValue}
+                        type="email"
+                        name="email"
+                        value={values.email}
                         label="Email Address"
                         variant="outlined"
                      />
@@ -28,13 +86,39 @@ const Login = () => {
                   <div className="log_input">
                      <TextField
                         id="outlined-basic"
+                        name="password"
+                        type={values.eye ? "text" : "password"}
                         label="Password"
                         variant="outlined"
+                        onChange={handleValue}
+                        value={values.password}
                      />
+                     <div
+                        onClick={() =>
+                           setValues({ ...values, eye: !values.eye })
+                        }
+                        className="log_eye"
+                     >
+                        {values.eye ? <FaRegEye /> : <FaRegEyeSlash />}
+                     </div>
                   </div>
-                  <Button className="log_btn" variant="contained">
-                     Login to continue
-                  </Button>
+                  <Alert style={{ marginBottom: "12px" }} severity="info">
+                     Not registerd ? Click here !{" "}
+                     <strong>
+                        <Link to="/">Registration</Link>
+                     </strong>
+                  </Alert>
+                  {values.loading ? (
+                     <CircularProgress />
+                  ) : (
+                     <Button
+                        onClick={Handlesubmit}
+                        className="log_btn"
+                        variant="contained"
+                     >
+                        Login to continue
+                     </Button>
+                  )}
                </div>
             </Grid>
             <Grid item xs={6}>
--- src/pages/Registation/Registration.jsx
+++ src/pages/Registation/Registration.jsx
@@ -1,23 +1,95 @@
 import React, { useState } from "react";
-import { Grid, TextField, Button } from "@mui/material";
+import {
+   Grid,
+   TextField,
+   Button,
+   CircularProgress,
+   Alert,
+} from "@mui/material";
+// import Alert from "@mui/material/Alert";
 import regimg from "../../assets/regi_img.png";
 import "../Registation/Registration.css";
+import {
+   getAuth,
+   createUserWithEmailAndPassword,
+   sendEmailVerification,
+} from "firebase/auth";
+import { useNavigate, Link } from "react-router-dom";
+import { FaRegEye, FaRegEyeSlash } from "react-icons/fa6";
 
 let initialValues = {
    email: "",
    fullName: "",
    password: "",
+   loading: false,
+   error: "",
+   eye: false,
 };
 
 const Registration = () => {
    let [values, setValues] = useState(initialValues);
+   const auth = getAuth();
+   let navigate = useNavigate();
+
    let handleValue = (e) => {
       setValues({
          ...values,
          [e.target.name]: e.target.value,
       });
-      console.log(values);
    };
+
+   let Handlesubmit = () => {
+      let { email, fullname, password } = values;
+
+      if (!email) {
+         setValues({
+            ...values,
+            error: "Enter a email",
+         });
+         return;
+      }
+      if (!fullname) {
+         setValues({
+            ...values,
+            error: "Enter your full-name",
+         });
+         return;
+      }
+
+      if (!/^[A-Za-z\s]+$/.test(fullname)) {
+         setValues({
+            ...values,
+            error: "Invalid characters in full name. Please use only letters and spaces.",
+         });
+         return;
+      }
+
+      if (!password) {
+         setValues({
+            ...values,
+            error: "Enter a password",
+         });
+         return;
+      }
+
+      setValues({
+         ...values,
+         loading: true,
+      });
+      createUserWithEmailAndPassword(auth, email, password).then((user) => {
+         sendEmailVerification(auth.currentUser).then(() => {
+            alert("verification send");
+         });
+         setValues({
+            email: "",
+            fullName: "",
+            password: "",
+            loading: false,
+         });
+         navigate("/login");
+      });
+   };
+
    return (
       <>
          <Grid container spacing={2} rowGap={0} columnGap={0}>
@@ -32,30 +104,68 @@ const Registration = () => {
                         name="email"
                         label="Email Address"
                         variant="outlined"
+                        value={values.email}
                      />
+                     {values.error.includes("email") && (
+                        <Alert severity="error">{values.error}</Alert>
+                     )}
                   </div>
                   <div className="reg_input">
                      <TextField
                         id="outlined-basic"
                         onChange={handleValue}
-                        name="fullName"
-                        label="Full Name"
+                        name="fullname"
+                        label="Fullname"
                         variant="outlined"
+                        value={values.fullname}
                      />
+                     {values.error.includes("full-name") && (
+                        <Alert severity="error">{values.error}</Alert>
+                     )}
+                     {values.error &&
+                        values.error.includes("Invalid characters") && (
+                           <Alert severity="error">{values.error}</Alert>
+                        )}
                   </div>
                   <div className="reg_input">
                      <TextField
                         id="outlined-basic"
-                        type="password"
+                        type={values.eye ? "text" : "password"}
                         onChange={handleValue}
                         name="password"
                         label="Password"
                         variant="outlined"
+                        value={values.password}
                      />
+                     {values.error.includes("password") && (
+                        <Alert severity="error">{values.error}</Alert>
+                     )}
+                     <div
+                        onClick={() =>
+                           setValues({ ...values, eye: !values.eye })
+                        }
+                        className="reg_eye"
+                     >
+                        {values.eye ? <FaRegEye /> : <FaRegEyeSlash />}
+                     </div>
                   </div>
-                  <Button className="reg_btn" variant="contained">
-                     Sign up
-                  </Button>
+                  <Alert style={{ marginBottom: "12px" }} severity="info">
+                     Already registerd then go to login !{" "}
+                     <strong>
+                        <Link to="/login">Login</Link>
+                     </strong>
+                  </Alert>
+                  {values.loading ? (
+                     <CircularProgress />
+                  ) : (
+                     <Button
+                        onClick={Handlesubmit}
+                        className="reg_btn"
+                        variant="contained"
+                     >
+                        Sign up
+                     </Button>
+                  )}
                </div>
             </Grid>
             <Grid item xs={6}>
